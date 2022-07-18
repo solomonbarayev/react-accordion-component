bebdb89f5fdf25ab10b26ff64a39542fb7dfa472
solomonbarayev <sbarayev2@gmail.com>
2022-07-18 13:06:23 +0300

    Updates

---
+++ precache-manifest.f592bc845948b1ba4a7df29717b70d4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14521967aacade113a561b71d97f581c",
+    "url": "/react-accordion-component/index.html"
+  },
+  {
+    "revision": "3afa1e952faf6a02b97d",
+    "url": "/react-accordion-component/static/css/main.4b589e59.chunk.css"
+  },
+  {
+    "revision": "11e2b97d487a12b7b136",
+    "url": "/react-accordion-component/static/js/2.f745c462.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-accordion-component/static/js/2.f745c462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3afa1e952faf6a02b97d",
+    "url": "/react-accordion-component/static/js/main.2ef02850.chunk.js"
+  },
+  {
+    "revision": "06e3a882eb1c3d05b63e",
+    "url": "/react-accordion-component/static/js/runtime-main.400741f2.js"
+  }
+]);
